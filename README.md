24192e0a861a9fd6d55223ae09c69902dc8703e9
Антон Пятков <antonpyatkov@MacBook-Air-Anton.local>
2020-01-22 03:38:47 +0400

    Add vue-waves-signer

--- README.md
+++ README.md
@@ -2,21 +2,4 @@
 
 [Demo](https://tltary.github.io/waves-signer-vue-example-demo)
 
-This example is for use with a test network.
-
-If you want to work with mainnet, go to /src/main.js, comment out the lines for the test network and uncomment for the mainnet
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```# waves-signer-vue-example
+This example is for [Vue Waves Signer](https://github.com/tltary/vue-waves-signer)
--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSigner from 'vue-waves-signer'
 
-import Signer from '@waves/signer';
-import Provider from '@waves.exchange/provider-web';
-
-// Testnet setup
-const signer = new Signer({ NODE_URL: 'https://pool.testnet.wavesnodes.com' });
-signer.setProvider(new Provider('https://testnet.waves.exchange/signer'));
-
-// Mainnet setup
-//const signer = new Signer();
-//signer.setProvider(new Provider());
-
-Vue.prototype.signer = signer;
+Vue.use(VueSigner, {
+  debug: true
+})
 
 Vue.config.productionTip = false
 
